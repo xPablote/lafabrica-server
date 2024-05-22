ded7827a87099452340d2c5eec3dd0d433a1ef09
Pablo villanea <pvillanea@gmail.com>
2024-05-22 14:11:24 -0400

    commit inicial

---
+++ src/main/java/cl/lafabrica/server/LafabricaServerApplication.java
@@ -0,0 +1,13 @@
+package cl.lafabrica.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafabricaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LafabricaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/lafabrica/server/LafabricaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.lafabrica.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafabricaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
